66e907e2ac92b3437b7bfff031c64e81f58b6210
Rupanshi15 <85381101+Rupanshi15@users.noreply.github.com>
2021-10-17 22:04:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var canvas, backgroundImage;
+var questions;
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(800,500);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.start();
+}
+
+function draw(){
+  background("pink");
+
+}
